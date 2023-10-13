5f2b70946492bd5bfb7fac8d9f0d95362c0e1552
Michael Eberhardt <github@michael-eberhardt.de>
2023-10-13 13:53:27 +0400

    typeinfo issue for v0.9.43

--- zenlink-protocol/rpc/runtime-api/src/lib.rs
+++ zenlink-protocol/rpc/runtime-api/src/lib.rs
@@ -6,16 +6,15 @@
 #![allow(clippy::unnecessary_mut_passed)]
 
 use codec::Codec;
-use scale_info::TypeInfo;
 use sp_std::vec::Vec;
 use zenlink_protocol::{AssetBalance, PairInfo};
 
 sp_api::decl_runtime_apis! {
 	 pub trait ZenlinkProtocolApi<AccountId, AssetId>
 	 where
-		AccountId: Codec + TypeInfo,
-		AssetBalance: Codec + TypeInfo,
-		AssetId: Codec + TypeInfo
+		AccountId: Codec,
+		AssetBalance: Codec,
+		AssetId: Codec
 	 {
 
 		fn get_balance(asset_id: AssetId, owner: AccountId) -> AssetBalance;
