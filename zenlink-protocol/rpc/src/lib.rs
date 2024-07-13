cd49595b1352c8fdbf87848f8bc837d2f066aab5
Jay Pan <pan@peaq.io>
2024-07-13 10:30:14 -0700

    Import scale-info to avoid issues

--- zenlink-protocol/rpc/src/lib.rs
+++ zenlink-protocol/rpc/src/lib.rs
@@ -12,7 +12,8 @@ use jsonrpsee::{
 	types::error::{CallError, ErrorObject},
 };
 
-use sp_api::{ProvideRuntimeApi, scale_info::TypeInfo};
+use sp_api::ProvideRuntimeApi;
+use scale_info::TypeInfo;
 use sp_blockchain::HeaderBackend;
 use sp_rpc::number::NumberOrHex;
 use sp_runtime::traits::Block as BlockT;
